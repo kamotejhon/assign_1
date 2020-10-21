5e46187fc38929a9c80e65adf5af47e43bd4ad95
kamotejhon <jhonbustamante011501@gmail.com>
2020-10-22 03:49:41 +0800

    changes

--- z.php
+++ z.php
@@ -7,6 +7,5 @@
     <h1><a href="http://localhost/assign_one/exercise1">exercise01</h1></p>
     <h1><a href="http://localhost/assign_one/exercise2">exercise02</h1></p>
     <h1><a href="http://localhost/assign_one/exercise3">exercise03</h1></p>
-
 </body>
 </html>
